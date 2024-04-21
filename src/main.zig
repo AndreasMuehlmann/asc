63aa06c2478f12042ce76dfb40ed20bbea2f1740
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-04-21 15:39:15 +0200

    testing gpio

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,27 @@
 const std = @import("std");
-const pigpio = @cImport({
+const gpio = @cImport({
     @cInclude("/home/andi/programming/nlslotter/libs/pigpio/pigpio.h");
 });
 
+fn button_callback(gpio_pin: c_int, level: c_int, ticks: u32) callconv(.C) void {
+    const stdout = std.io.getStdOut().writer();
+    stdout.print("interrupt gpio_pin {} level {} ticks {}", .{ gpio_pin, level, ticks }) catch return;
+}
+
 pub fn main() !void {
-    _ = pigpio.gpioInitialise();
-    defer pigpio.gpioTerminate();
+    _ = gpio.gpioInitialise();
+    defer gpio.gpioTerminate();
 
-    _ = pigpio.gpioSetMode(18, pigpio.PI_OUTPUT);
-    _ = pigpio.gpioWrite(18, 1);
+    _ = gpio.gpioSetMode(3, gpio.PI_OUTPUT);
+    _ = gpio.gpioWrite(3, 1);
+    std.time.sleep(1_000_000_000);
+    _ = gpio.gpioWrite(3, 0);
+    std.time.sleep(1_000_000_000);
+    _ = gpio.gpioWrite(3, 1);
+    std.time.sleep(1_000_000_000);
+    _ = gpio.gpioWrite(3, 0);
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush();
+    _ = gpio.gpioSetMode(2, gpio.PI_INPUT);
+    _ = gpio.gpioSetISRFunc(2, gpio.FALLING_EDGE, 0, button_callback);
+    std.time.sleep(30_000_000_000);
 }
