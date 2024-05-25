c0a1a67edf16793c86971bdf6109c97e6203f1c2
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-05-25 16:25:46 +0200

    cross-compilation working with libraries (not cmake)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <unistd.h>
+#include <assert.h>
 
-// #include <zmq.h>
+#include <zmq.h>
 #include <pigpio.h>
-
 /*
 extern "C"
 void button_callback(int gpio_pin, int level, unsigned int ticks) {
@@ -12,7 +12,21 @@ void button_callback(int gpio_pin, int level, unsigned int ticks) {
 */
 
 int main() {
-    std::cout << "Hello World!" << std::endl;    
+    void *context = zmq_ctx_new();
+    std::cout << "Hello World!" << std::endl;
+    /*
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind (responder, "tcp://*:5555");
+    assert(rc == 0);
+
+    while (1) {
+        char buffer[10];
+        zmq_recv(responder, buffer, 10, 0);
+        std::cout << ("Received Hello\n") << std::endl;
+        sleep(1);
+        zmq_send(responder, "World", 5, 0);
+    }
+    */
     gpioInitialise();
     gpioSetMode(3, PI_OUTPUT);
     gpioWrite(3, 1);
@@ -26,4 +40,5 @@ int main() {
     // gpioSetMode(2, PI_INPUT);
     // gpioSetISRFunc(2, FALLING_EDGE, 0, button_callback);
     gpioTerminate();
+    return 0;
 }
