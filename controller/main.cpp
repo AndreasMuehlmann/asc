2f422d233715face36c6923c4313e10b79bdf96b
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-06-02 11:55:22 +0200

    use bno driver and enable debug mode

--- controller/main.cpp
+++ controller/main.cpp
@@ -1,13 +1,78 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 
 #include "zmq.hpp"
 #include "commands.pb.h"
 
+#include "pigpio.h"
+extern "C" {
+    #include "bno055.h"
+}
+
+
+
 using namespace std::chrono_literals;
 
-int main () {
-     
+#define BUS 1
+
+void delay_msec(uint32_t msec) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(msec));
+}
+
+s8 i2c_read(u8 dev_id, u8 reg_addr, u8 *data, u8 len) {
+    std::cout << "i2c_read" << std::endl;
+    int handle = i2cOpen(BUS, dev_id, 0);
+    if (handle == 1) {
+        return -1;
+    }
+
+    int8_t result = i2cReadI2CBlockData(handle, reg_addr, reinterpret_cast<char*>(data), len);
+    i2cClose(handle);
+
+    return result;
+}
+
+s8 i2c_write(u8 dev_id, u8 reg_addr, u8 *data, u8 len) {
+    std::cout << "i2c_write" << std::endl;
+    int handle = i2cOpen(BUS, dev_id, 0);
+    if (handle < 0) {
+        return -1;
+    }
+
+    char buf[256];
+    buf[0] = reg_addr;
+    memcpy(buf + 1, data, len);
+
+    int8_t result = i2cWriteI2CBlockData(handle, reg_addr, buf, len + 1);
+    i2cClose(handle);
+
+    return result;
+}
+
+int main() {
+    if (gpioInitialise() < 0) {
+        std::cerr << "pigpio initialization failed." << std::endl;
+        return 1;
+    }
+    struct bno055_t bno055;
+    bno055.bus_read = i2c_read;
+    bno055.bus_write = i2c_write;
+    bno055.delay_msec = delay_msec;
+    bno055.dev_addr = BNO055_I2C_ADDR1;
+
+    bno055_init(&bno055);
+    struct bno055_euler_t *euler;
+    std::cout << "before hrp" << std::endl;
+    bno055_read_euler_hrp(euler);
+    std::cout << "euler r: " << euler->r << std::endl;
+    bno055_read_euler_hrp(euler);
+    std::cout << "euler r: " << euler->r << std::endl;
+    bno055_read_euler_hrp(euler);
+    std::cout << "euler r: " << euler->r << std::endl;
+    
+    gpioTerminate();
+
     proto::Command command;
     command.set_msg("Hello World!!!");
     std::string actual_message;
